a2b7163066f73d42d21af10d41bbdba57d1b97fa
kukujiabo <kukujiabo@163.com>
2018-06-04 21:53:20 +0800

    123

--- src/app/Api/Merchant.php
+++ src/app/Api/Merchant.php
@@ -15,10 +15,10 @@ class Merchant extends BaseApi {
       'create' => [
 
         'mcode' => 'mcode|string|true||商户号',
-        'phone' => 'phone|string|true||商户手机号',
         'mname' => 'mname|string|true||商家名称',
-        'brief' => 'brief|string|true||商家简介',
         'thumbnail' => 'thumbnail|string|true||商家头像',
+        'brief' => 'brief|string|false||商家简介',
+        'phone' => 'phone|string|false||商户手机号',
         'image_text' => 'image_text|string|false||商家图文详情',
         'carousel' => 'carousel|string|false||商家轮播图',
         'status' => 'status|int|false|1|商户状态'
